5a2affbe968a5a6c189caabacb0178c732d46be2
Hassan Khan <me@hassankhan.me>
2013-11-28 00:40:10 +0000

    Really rough first implementation of ``Zepto\FileWriter`` and ``Zepto\FileWriter\MarkdownLoader``.

---
+++ library/Zepto/FileWriter.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * FileWriter
+ *
+ * @author Hassan Khan
+ * @link http://https://github.com/hassankhan/Zepto
+ * @license http://opensource.org/licenses/MIT
+ * @version 0.5
+ */
+
+namespace Zepto;
+
+class FileWriter {
+
+    public function __construct()
+    {
+
+    }
+
+    // Seems to work, meh
+    public function write($file_path, $file_data)
+    {
+        $handle = fopen($file_path, 'w');
+        if ($handle === false) {
+            throw new Exception(
+                'Failed to create file ' . $file_name
+                . 'at ' . $file_path
+            );
+        }
+
+        if (fwrite($handle, $file_data) === false) {
+            throw new Exception(
+                'Failed to create file ' . $file_name
+                . 'at ' . $file_path
+            );
+        }
+
+        return true;
+    }
+
+}
---
+++ library/Zepto/FileWriter/MarkdownWriter.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * MarkdownWriter
+ *
+ * @author Hassan Khan
+ * @link http://https://github.com/hassankhan/Zepto
+ * @license http://opensource.org/licenses/MIT
+ * @version 0.5
+ */
+
+namespace Zepto\FileWriter;
+
+class MarkdownWriter extends \Zepto\FileWriter {
+
+    protected static $file_template = <<<MARKDOWN
+/*
+Title: %s
+Description: %s
+Date: %s
+
+%s
+*/
+MARKDOWN;
+
+    public function __construct()
+    {
+
+    }
+
+    // Seems to work, meh
+    public function write($file_path, $file_data)
+    {
+        list($title, $date, $content) = $file_data;
+
+        $clean_title   = htmlentities($title,   ENT_HTML5, 'UTF-8');
+        $clean_date    = htmlentities($date,    ENT_HTML5, 'UTF-8');
+        $clean_content = htmlentities($content, ENT_HTML5, 'UTF-8');
+
+        $data          = array(
+            $clean_title,
+            $clean_date,
+            $clean_content
+        );
+
+        // Put text in Markdown template
+        $file_data     = vsprintf($this->file_template, $data);
+
+        // Call parent method to do write
+        return parent::write($file_path, $file_data);
+    }
+
+}
