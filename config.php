afbf64a96ffb027dd3ac5988dd7e8fb7df9db6e0
Hassan Khan <me@hassankhan.me>
2014-01-13 22:50:43 +0000

    Added new options to config file

--- config.php
+++ config.php
@@ -13,7 +13,12 @@ $config = array(
         'site_root'         => 'Site root URL goes here',
         'site_title'        => 'Zepto',
         'date_format'       => 'jS M Y',
-        'excerpt_length'    => '50'
+        'excerpt_length'    => '50',
+        'nav'               => array(
+            'class'             => 'nav',
+            'dropdown_li_class' => 'dropdown',
+            'dropdown_ul_class' => 'dropdown-menu'
+        )
     ),
     'twig' => array(
         'charset'           => 'utf-8',
@@ -25,26 +30,3 @@ $config = array(
 );
 
 return $config;
-
-
-/*
-// Override any of the default settings below:
-
-$config['site_title'] = 'Pico';			// Site title
-$config['base_url'] = ''; 				// Override base URL (e.g. http://example.com)
-$config['theme'] = 'default'; 			// Set the theme (defaults to "default")
-$config['date_format'] = 'jS M Y';		// Set the PHP date format
-$config['twig_config'] = array(			// Twig settings
-	'cache' => false,					// To enable Twig caching change this to CACHE_DIR
-	'autoescape' => false,				// Autoescape Twig vars
-	'debug' => false					// Enable Twig debug
-);
-$config['pages_order_by'] = 'alpha';	// Order pages by "alpha" or "date"
-$config['pages_order'] = 'asc';			// Order pages "asc" or "desc"
-$config['excerpt_length'] = 50;			// The pages excerpt length (in words)
-
-// To add a custom config setting:
-
-$config['custom_setting'] = 'Hello'; 	// Can be accessed by {{ config.custom_setting }} in a theme
-
-*/
