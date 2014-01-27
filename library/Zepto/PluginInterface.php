50944db0d2e849c03c4969ed2364cc9f4663d2ac
Hassan Khan <me@hassankhan.me>
2014-01-27 01:43:28 +0000

    ``Zepto\PluginInterface::after_config_load()`` is now ``before_config_load()``, allows for a chance to hook into app settings before they are fully loaded. Updated ExamplePlugin and OtherExamplePlugin to show changes

--- library/Zepto/PluginInterface.php
+++ library/Zepto/PluginInterface.php
@@ -13,27 +13,14 @@ namespace Zepto;
 
 interface PluginInterface {
 
-    public function after_config_load(&$settings);
-
     public function after_plugins_load();
+
+    public function before_config_load(&$settings);
 
     public function before_file_load(&$content_dir);
 
     public function after_file_load(&$content);
 
     public function request_url(&$url);
-
-    // Move all these methods into their own classes
-    // public function before_file_meta_parsed(&$headers);
-
-    // public function after_file_meta_parsed(&$meta);
-
-    // public function before_content_parsed(&$content);
-
-    // public function after_content_parsed(&$content);
-
-    // public function before_render(&$twig_vars, &$twig, &$template);
-
-    // public function after_render(&$output);
 
 }
