72b8741b0b6912a7a64e23512b089f826940f19e
Hassan Khan <me@hassankhan.me>
2014-02-02 04:26:17 +0000

    Added a new method ``Zepto\Zepto::default_config()``

--- library/Zepto/Zepto.php
+++ library/Zepto/Zepto.php
@@ -115,6 +115,11 @@ class Zepto {
             }
         );
 
+        // If settings array is empty, then get a default one
+        $settings = empty($settings) === true
+            ? $settings = $this->default_config()
+            : $settings;
+
         // Set this particular setting now
         $container['plugins_enabled'] = $settings['zepto']['plugins_enabled'];
 
@@ -284,10 +289,6 @@ class Zepto {
                     ? $content['meta']['template']
                     : $container['settings']['zepto']['default_template'];
 
-                if ($file === '404') {
-                    header("HTTP/1.0 404 Not Found");
-                }
-
                 // Render template with Twig
                 return $twig->render($template_name, $options);
             });
@@ -380,7 +381,41 @@ class Zepto {
         return $nav_html;
     }
 
+    /**
+     * Returns a standard configuration for Zepto
+     *
+     * @return array
+     */
+    protected static function default_config()
     {
+        return array(
+            'zepto' => array(
+                'content_dir'       => 'content',
+                'plugins_dir'       => 'plugins',
+                'templates_dir'     => 'templates',
+                'default_template'  => 'base.twig',
+                'content_ext'       => array('.md', '.markdown'),
+                'plugins_enabled'   => false
+            ),
+            'site' => array(
+                'site_root'         => 'http://localhost:8888/zepto/',
+                'site_title'        => 'Zepto',
+                'date_format'       => 'jS M Y',
+                'excerpt_length'    => '50',
+                'nav'               => array(
+                    'class'             => 'nav',
+                    'dropdown_li_class' => 'dropdown',
+                    'dropdown_ul_class' => 'dropdown-menu'
+                )
+            ),
+            'twig' => array(
+                'charset'           => 'utf-8',
+                'cache'             => 'cache',
+                'strict_variables'  => false,
+                'autoescape'        => false,
+                'auto_reload'       => true
+            )
+        );
     }
 
 }
