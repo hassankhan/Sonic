ef77b7d2353d24e17b3e58772f16c71ca042efa8
Hassan Khan <me@hassankhan.me>
2014-02-11 20:50:44 +0000

    Created new ``NavGenPlugin``

---
+++ plugins/NavGenPlugin.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * NavGen plugin
+ *
+ * Creates a lovely formatted version of the
+ *
+ * @author Hassan Khan
+ * @link http://https://github.com/hassankhan/Zepto
+ * @license http://opensource.org/licenses/MIT
+ * @version 0.6
+ */
+
+class NavGenPlugin implements \Zepto\PluginInterface {
+
+    public function after_plugins_load()
+    {
+    }
+
+    public function before_config_load(&$settings)
+    {
+    }
+
+    public function before_router_setup()
+    {
+    }
+
+    public function after_router_setup()
+    {
+    }
+
+    public function before_response_send()
+    {
+    }
+
+    public function after_response_send()
+    {
+    }
+
+}
+
+?>
