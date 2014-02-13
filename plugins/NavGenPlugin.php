48cbed7e987e9f94cd3ce933adc76c37882c70d8
Hassan Khan <me@hassankhan.me>
2014-02-13 20:25:29 +0000

    Initial, somewhat working version of ``NavGenPlugin``

--- plugins/NavGenPlugin.php
+++ plugins/NavGenPlugin.php
@@ -27,9 +27,9 @@ class NavGenPlugin implements \Zepto\PluginInterface {
     public function after_router_setup()
     {
         // Use this one
-        $args = func_get_args();
-        $html = $this->generate_html($arg[0]);
-        $app['nav'] = $html;
+        $app = func_get_arg(0);
+        $html = $this->generate_html($app);
+        $app['nav'] = array('nav' => $html);
     }
 
     public function before_response_send()
@@ -46,64 +46,19 @@ class NavGenPlugin implements \Zepto\PluginInterface {
         $content_loader  = $app['content_loader'];
 
         // Opening ``<ul>`` tag and adding class name
-        $nav_html     = sprintf('<ul class="%s">' . PHP_EOL, $settings['site']['nav']['class']);
+        $nav_html = sprintf('<ul class="%s">' . PHP_EOL, $settings['site']['nav']['class']);
+        $files    = $content_loader->get_folder_contents();
 
-        $structure    = $content_loader->get_folder_contents($settings['zepto']['content_dir']);
+        foreach ($files as $file) {
+            $exploded_file_name = explode('/', $file);
 
-        // Remove 'index' along with any file extensions from URL
-        // $filth = array_merge(array('index'), $settings['zepto']['content_ext']);
-
-        foreach ($structure as $value) {
-
-            // Check if ``$value`` is an array
-            if (is_array($value)) {
-
-                // Generate HTML for dropdown menu
-                $dropdown_html = '<li class="%s">' . PHP_EOL
-                   . '<a href="%s" class="dropdown-toggle" data-toggle="dropdown"> %s <b class="caret"></b></a>' . PHP_EOL
-                   . '<ul class="%s">' . PHP_EOL;
-                $nav_html .= sprintf($dropdown_html,
-                    $settings['site']['nav']['dropdown_li_class'],
-                    reset($value), // Reset to get first value from array
-                    ucfirst($key), // Capitalise first letter of folder name
-                    $settings['site']['nav']['dropdown_ul_class']
-                );
-
-                foreach ($value as $file_name) {
-
-                    // Add folder name to file name
-                    $full_file_name = $key . '/' . $file_name;
-
-                    // Get title of content file
-                    $title = $app['content'][$full_file_name]['meta']['title'];
-
-                    // Create URL
-                    $url = $settings['site']['site_root'] . str_replace($filth, '', $key . '/' . $file_name);
-
-                    // Run ``ucfirst()`` on ``$key`` to make it look nice
-                    $nav_html  .= sprintf('<li><a href="%s"> %s </a></li>' . PHP_EOL, $url, $title);
-                }
-
-                // Close dropdown menu HTML tags
-                $nav_html .= '</ul></li>' . PHP_EOL;
-            }
-            // If not then add to ``$nav_items``
-            else {
-                if (preg_match('#^[4|5]0\d\.md$#i', $value) === 0) {
-
-                    // Get title of content file
-                    $title = $app['content'][$value]['meta']['title'];
-
-                    // Create URL
-                    $url = $settings['site']['site_root'] . str_replace($filth, '', $value);
-                    $url = rtrim($url, '/') . '/';
-
-                    $nav_html .= sprintf('<li><a href="%s"> %s </a></li>' . PHP_EOL, $url, $title);
-                }
+            // Top-level links
+            if (count($exploded_file_name) === 1) {
+                $nav_html .= '<li>' . $app['helper']->link_for($file) . '</li>' . PHP_EOL;
+                continue;
             }
         }
 
-        // Close ``<ul>`` tag
         $nav_html .= '</ul>' . PHP_EOL;
         return $nav_html;
     }
