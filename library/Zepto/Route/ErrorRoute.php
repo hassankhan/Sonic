7c9b982419325defa3a4ba80fe6311d118e1b988
Hassan Khan <me@hassankhan.me>
2014-02-17 02:54:34 +0000

    Added new object ``Zepto\Route\ErrorRoute``

---
+++ library/Zepto/Route/ErrorRoute.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Zepto\Route;
+
+/**
+ * ErrorRoute
+ *
+ * @package    Zepto
+ * @subpackage Route
+ * @author     Hassan Khan <contact@hassankhan.me>
+ * @link       https://github.com/hassankhan/Zepto
+ * @license    MIT
+ * @since      0.7
+ */
+class ErrorRoute
+{
+
+    /**
+     * Initializes the route by creating a regex pattern from the provided URL,
+     * and assigns the callable function for this route
+     *
+     * @param string $url
+     * @codeCoverageIgnore
+     */
+    public function __construct($url = '')
+    {
+        $callback = function () use ($this) {
+            return $this->generate_error_template('')
+        };
+    }
+
+    /**
+     * Executes a route's callback and returns the result. Simple as that.
+     *
+     * @param  array  $params
+     * @return string
+     */
+    public function execute($params = array())
+    {
+        return call_user_func_array($this->callback, $params);
+    }
+
+    /**
+     * ACCESSORS
+     */
+
+    /**
+     * Returns a standard template for error messages. Thanks, Slim
+     *
+     * @param  string $title
+     * @param  string $body
+     * @return string
+     * @codeCoverageIgnore
+     */
+    protected function generate_error_template($title, $body)
+    {
+        return sprintf('<html><head><title>%s</title><style>body{margin:0;padding:30px;font:12px/1.5 Helvetica,Arial,Verdana,sans-serif;}h1{margin:0;font-size:48px;font-weight:normal;line-height:48px;}strong{display:inline-block;width:65px;}</style></head><body><h1>%s</h1><p>%s</p></body></html>', $title, $title, $body);
+    }
+}
