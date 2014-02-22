04113d3a0a0f6fc3521f585457b8b42ca2c48b37
Hassan Khan <me@hassankhan.me>
2014-02-22 19:01:49 +0000

    Removed error reporting stuff from index.php

--- index.php
+++ index.php
@@ -4,9 +4,5 @@
 require('vendor/autoload.php' );
 require('config.php');
 
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(-1);
-
 $zepto = new Zepto\Zepto($config);
 $zepto->run();
