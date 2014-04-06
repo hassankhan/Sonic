a3e1a5ba28baa9c445e99ca23b3fda1a808c7a1d
Hassan Khan <me@hassankhan.me>
2014-04-06 16:27:07 +0100

    Created ``Zepto\Route\ListRouteAbstract`` which seems to make it more DRY. Also updated ``Zepto\Route\ListRoute`` and ``Zepto\Route\TagRoute``

--- library/Zepto/Route/ListRoute.php
+++ library/Zepto/Route/ListRoute.php
@@ -14,7 +14,7 @@ namespace Zepto\Route;
  * @license    MIT
  * @since      0.7
  */
-class ListRoute extends \Zepto\Route
+class ListRoute extends \Zepto\Route\ListRouteAbstract
 {
 
     /**
@@ -29,60 +29,12 @@ class ListRoute extends \Zepto\Route
         parent::__construct($url, array($this, 'build_route'));
     }
 
-    /**
-     * Builds and returns the rendered HTML
-     *
-     * @return string
-     */
-    public function build_route($param = '')
+    public function posts()
     {
-        // Get reference to Zepto
-        $zepto = \Zepto\Zepto::instance();
         // Get dates from all content
-        $dates = $zepto->app['filesystem']->dates('content');
-        // Get filenames of content and create array to hold posts
-        $posts = $this->get_excerpts(array_keys($dates));
+        $dates = $this->zepto->app['filesystem']->dates('content');
 
-        // Load in any extra stuffs
-        $zepto->app['extra'] = isset($zepto->app['extra']) === TRUE ? $zepto->app['extra'] : array();
-
-        // Render template with Twig
-        return $zepto->app['twig']->render($zepto->app['settings']['zepto.default_list_template'], array('contents' => $posts));
-    }
-
-    /**
-     * Gets file excerpts from all files
-     *
-     * @param  array $files
-     * @return array
-     */
-    public function get_excerpts($files)
-    {
-        // Get reference to Zepto
-        $zepto = \Zepto\Zepto::instance();
-        // Create array to hold posts
-        $posts = array();
-
-        // Iterate through files and get excerpts for all of them
-        foreach ($files as $file) {
-            $file_contents = $zepto->app['filesystem']->parse($file);
-
-            if (
-                $file_contents['meta']['title'] !== 'Quote'
-                &&
-                $zepto->app['settings']['site.excerpt_newline_limit'] !== 0
-            ) {
-                $contents  = $zepto->app['helper']->get_excerpt(
-                    $file_contents['contents'],
-                    $zepto->app['settings']['site.excerpt_newline_limit']
-                );
-
-                $file_contents['contents'] = $contents;
-            }
-            $posts[str_replace('content/', '', $file)] = $file_contents;
-        }
-
-        return $posts;
+        return $this->excerpts(array_keys($dates));
     }
 
 }
--- library/Zepto/Route/TagRoute.php
+++ library/Zepto/Route/TagRoute.php
@@ -29,29 +29,15 @@ class TagRoute extends \Zepto\Route\ListRoute
         parent::__construct($url, array($this, 'build_route'));
     }
 
-    /**
-     * Builds and returns the rendered HTML
-     *
-     * @return string
-     */
-    public function build_route($tag_name = '')
+    public function posts()
     {
-        // Get reference to Zepto
-        $zepto = \Zepto\Zepto::instance();
+        $params = func_get_args();
+        $tag_name = $params[0];
         // Get tags
-        $tags           = $zepto->app['filesystem']->tags('content');
-        // Load file
+        $tags           = $this->zepto->app['filesystem']->tags('content');
         $tagged_files   = $tags[$tag_name];
-        // Create array to hold posts
-        $posts          = $this->get_excerpts($tagged_files);
-        // Load in any extra stuffs
-        $zepto->app['extra'] = isset($zepto->app['extra']) === TRUE ? $zepto->app['extra'] : array();
 
-        // Merge Twig options and content into one array
-        $options             = array_merge($posts, $zepto->app['extra']);
-
-        // Render template with Twig
-        return $zepto->app['twig']->render($zepto->app['settings']['zepto.default_list_template'], array('contents' => $posts));
+        return $this->excerpts($tagged_files);
     }
 
 }
